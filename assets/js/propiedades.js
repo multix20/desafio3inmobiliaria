815e141b2f9abcbfd44899815497919d7e2e011e
multix20 <multix2009@gmail.com>
2024-06-20 20:32:45 -0400

    subiendo deasfio

---
+++ assets/js/propiedades.js
@@ -0,0 +1,87 @@
+// propiedades.js
+const propiedades_venta = [
+  {
+    nombre: 'Casa en la playa',
+    src: 'https://images.unsplash.com/photo-1615571022219-eb45cf7faa9d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Hermosa casa frente al mar.',
+    ubicacion: 'Playa del Sol',
+    habitaciones: 3,
+    costo: 300000,
+    smoke: true,
+    pets: false
+  },
+  {
+    nombre: 'Departamento en la ciudad',
+    src: 'https://images.unsplash.com/photo-1515263487990-61b07816b324?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Moderno departamento en el centro.',
+    ubicacion: 'Ciudad Central',
+    habitaciones: 2,
+    costo: 200000,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Casa en el campo',
+    src: 'https://images.unsplash.com/photo-1573652102907-b75d25910c11?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Tranquila casa en el campo.',
+    ubicacion: 'Campo Verde',
+    habitaciones: 4,
+    costo: 250000,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Apartamento en el centro',
+    src: 'https://images.unsplash.com/photo-1595481857271-53daca8f91b4',
+    descripcion: 'Apartamento moderno cerca de todas las comodidades.',
+    ubicacion: 'Ciudad Vieja',
+    habitaciones: 2,
+    costo: 150000,
+    smoke: true,
+    pets: false
+  }
+];
+
+const propiedades_alquiler = [
+  {
+    nombre: 'Casa en las montañas',
+    src: 'https://plus.unsplash.com/premium_photo-1663045246303-9f60f7a62567?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Acogedora casa en las montañas.',
+    ubicacion: 'Montañas Verdes',
+    habitaciones: 4,
+    costo: 1500,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Departamento en la playa',
+    src: 'https://plus.unsplash.com/premium_photo-1661962733434-0c3d4ca3ec0b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Departamento con vista al mar.',
+    ubicacion: 'Playa del Sol',
+    habitaciones: 3,
+    costo: 1200,
+    smoke: true,
+    pets: false
+  },
+  {
+    nombre: 'Casa en el bosque',
+    src: 'https://plus.unsplash.com/premium_photo-1686090449192-4ab1d00cb735?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    descripcion: 'Casa rodeada de naturaleza.',
+    ubicacion: 'Bosque Encantado',
+    habitaciones: 3,
+    costo: 1300,
+    smoke: true,
+    pets: true
+  },
+  {
+    nombre: 'Apartamento en el centro',
+    src: 'https://images.unsplash.com/photo-1502672023488-70e25813eb80',
+    descripcion: 'Apartamento en el corazón de la ciudad.',
+    ubicacion: 'Ciudad Central',
+    habitaciones: 2,
+    costo: 900,
+    smoke: false,
+    pets: false
+  }
+];
+
